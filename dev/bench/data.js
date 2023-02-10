bbcbc8e491b7843ca54a540051a5ab04d5f7c832
github-action-benchmark <github@users.noreply.github.com>
2023-02-10 18:13:25 +0000

    add internals benchmarks (go) benchmark result for e9e5a24bd5ee411456384c836e46d1a2e2438faf

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1676052266589,
+  "lastUpdate": 1676052805977,
   "repoUrl": "https://github.com/PandaSekh/dibbi",
   "entries": {
     "internals benchmarks": [
@@ -108,6 +108,42 @@ window.BENCHMARK_DATA = {
             "value": 58476,
             "unit": "ns/op",
             "extra": "20424 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alessiofranceschi2@gmail.com",
+            "name": "Alessio Franceschi",
+            "username": "PandaSekh"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e9e5a24bd5ee411456384c836e46d1a2e2438faf",
+          "message": "Merge pull request #1 from PandaSekh/add-license-1\n\nCreate LICENSE.md",
+          "timestamp": "2023-02-10T19:12:58+01:00",
+          "tree_id": "e353ea37e856a2d169345e5b5c09f0de9a82b042",
+          "url": "https://github.com/PandaSekh/dibbi/commit/e9e5a24bd5ee411456384c836e46d1a2e2438faf"
+        },
+        "date": 1676052805213,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLex/input_length_49",
+            "value": 20051,
+            "unit": "ns/op",
+            "extra": "57864 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkLex/input_length_136",
+            "value": 63640,
+            "unit": "ns/op",
+            "extra": "19164 times\n2 procs"
           }
         ]
       }
