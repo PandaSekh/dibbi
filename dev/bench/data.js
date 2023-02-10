0d6caf279a7718de4d268cdae7c140492baaf4a4
github-action-benchmark <github@users.noreply.github.com>
2023-02-10 17:49:39 +0000

    add internals benchmarks (go) benchmark result for 371a2bb4be4b7c2696d486a5a7e5eb16fa5867bd

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1676051379381,
+  "repoUrl": "https://github.com/PandaSekh/dibbi",
+  "entries": {
+    "internals benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "alessiofranceschi2@gmail.com",
+            "name": "Alessio Franceschi",
+            "username": "PandaSekh"
+          },
+          "committer": {
+            "email": "alessiofranceschi2@gmail.com",
+            "name": "Alessio Franceschi",
+            "username": "PandaSekh"
+          },
+          "distinct": true,
+          "id": "371a2bb4be4b7c2696d486a5a7e5eb16fa5867bd",
+          "message": "Merge branch 'feature/initial_state' into develop",
+          "timestamp": "2023-02-10T18:45:10+01:00",
+          "tree_id": "05c6f0168f5ac33b01eb652185cb286da9937729",
+          "url": "https://github.com/PandaSekh/dibbi/commit/371a2bb4be4b7c2696d486a5a7e5eb16fa5867bd"
+        },
+        "date": 1676051377943,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLex/input_length_49",
+            "value": 24562,
+            "unit": "ns/op",
+            "extra": "49497 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkLex/input_length_136",
+            "value": 76868,
+            "unit": "ns/op",
+            "extra": "16078 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
