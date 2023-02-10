dfb52e2a605476e664abadd4a0c8d6f722ab58df
github-action-benchmark <github@users.noreply.github.com>
2023-02-10 18:02:06 +0000

    add internals benchmarks (go) benchmark result for f5c60a647964e9f29f8e01a5b0ba77765357f824

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1676051379381,
+  "lastUpdate": 1676052126877,
   "repoUrl": "https://github.com/PandaSekh/dibbi",
   "entries": {
     "internals benchmarks": [
@@ -36,6 +36,42 @@ window.BENCHMARK_DATA = {
             "value": 76868,
             "unit": "ns/op",
             "extra": "16078 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "alessiofranceschi2@gmail.com",
+            "name": "Alessio Franceschi",
+            "username": "PandaSekh"
+          },
+          "committer": {
+            "email": "alessiofranceschi2@gmail.com",
+            "name": "Alessio Franceschi",
+            "username": "PandaSekh"
+          },
+          "distinct": true,
+          "id": "f5c60a647964e9f29f8e01a5b0ba77765357f824",
+          "message": "Updated readme\n\nSigned-off-by: Alessio Franceschi <alessiofranceschi2@gmail.com>",
+          "timestamp": "2023-02-10T19:01:39+01:00",
+          "tree_id": "e29c318cb98b3ece10ad1abbdaaae61aa7a7d430",
+          "url": "https://github.com/PandaSekh/dibbi/commit/f5c60a647964e9f29f8e01a5b0ba77765357f824"
+        },
+        "date": 1676052125715,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkLex/input_length_49",
+            "value": 18825,
+            "unit": "ns/op",
+            "extra": "63519 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkLex/input_length_136",
+            "value": 58433,
+            "unit": "ns/op",
+            "extra": "20574 times\n2 procs"
           }
         ]
       }
